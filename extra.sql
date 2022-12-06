690625b703651ba0dea0f48a4361c42f8391b6e9
Pavlos Poulos <81812817+PavlosPo@users.noreply.github.com>
2022-12-06 14:37:11 +0200

    Extreme Poverty and GDP added as Queries

--- extra.sql
+++ extra.sql
@@ -6,8 +6,11 @@ GROUP BY iso_code, location, population
 ORDER BY Deaths DESC
 
 
--- Death Per Million to correlate high-low income Deaths.
 -- Found that High income Countries have more Deaths Per million opposed to Low Income.
+
+
+
+-- Continent
 SELECT 
 	iso_code,
 	location,
@@ -15,16 +18,37 @@ SELECT
 	MAX(total_deaths_per_million) As Max_DeathsPerMillion,
 	AVG(total_deaths_per_million) As AVG_DeathsPerMillion
 FROM PortfolioProject.dbo.owid_covid_data ocd 
-WHERE ocd.iso_code LIKE 'OWID_%' AND location IN (
+WHERE ocd.iso_code LIKE 'OWID_%' OR location IN (
 										'High income', 
 										'Upper middle income',
 										'Lower middle income')
-GROUP BY iso_code, location
-ORDER BY Deaths DESC
+GROUP BY iso_code, location, aged_65_older 
+ORDER BY AVG_DeathsPerMillion DESC
  
 
+SELECT 
+	location,
+	gdp_per_capita as GDP,
+	CAST(extreme_poverty as float) ExtremePoverty,
+	SUM(new_deaths) As Deaths,
+	MAX(total_deaths_per_million) As Max_DeathsPerMillion,
+	AVG(total_deaths_per_million) As AVG_DeathsPerMillion
+FROM PortfolioProject.dbo.owid_covid_data ocd 
+WHERE continent NOT LIKE ''
+GROUP BY location, gdp_per_capita, extreme_poverty 
+ORDER BY CAST(extreme_poverty as float) DESC
+
+------------------------------------------------------------------------------------------------
+-- Question: Policy played role? argument variable: stringency_index
+SELECT iso_code, location, AVG(stringency_index)
+FROM PortfolioProject.dbo.owid_covid_data ocd 
+WHERE iso_code LIKE 'OWID_%'
+GROUP BY location, gdp_per_capita 
 
 
+
+
+-- Question: How ICU helped people, did they recover? or high percentage died?
 -- Question: How many days were borders in closure on high-middle and low income countries?
 
 
