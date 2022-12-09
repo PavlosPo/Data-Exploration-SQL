c0d0774fcc7d6add6c0668a423319ca82139c3aa
Pavlos Poulos <81812817+PavlosPo@users.noreply.github.com>
2022-12-09 18:45:18 +0200

    Created more tamp tables about important info

--- extra.sql
+++ extra.sql
@@ -1,12 +1,5 @@
--- Continents View
-SELECT iso_code, location, population, SUM(new_deaths) As Deaths
-FROM PortfolioProject.dbo.owid_covid_data ocd 
-WHERE ocd.iso_code LIKE 'OWID_%'
-GROUP BY iso_code, location, population
-ORDER BY Deaths DESC
-
-
--- Found that High income Countries have more Deaths Per million opposed to Low Income.
+-- Found that High income Countries have more 
+-- Deaths Per million opposed to Low Income.
 
 
 
@@ -46,7 +39,7 @@ FROM PortfolioProject.dbo.international_travel_covid itc
 GROUP BY location
 
 
--- 4.Temp Table with Restriction Power and Days
+-- 4.Temp Table with Restriction Power and DaysInRestrictions
 DROP TABLE IF EXISTS #restrict_table
 CREATE TABLE #restrict_table( 	
 						location varchar(50),
@@ -58,30 +51,62 @@ INSERT INTO  #restrict_table
 SELECT 
 	ocd.location,
 	itc.international_travel_controls As RestrictionPower,
-	COUNT( DISTINCT itc.date ) As DaysInREstrictions
+	COUNT( DISTINCT itc.date ) As DaysInRestrictions
 FROM PortfolioProject.dbo.owid_covid_data ocd 
 LEFT JOIN PortfolioProject.dbo.international_travel_covid itc 
 	ON ocd.location = itc.location 
 GROUP BY ocd.location, itc.international_travel_controls
 ORDER BY location, RestrictionPower ASC
 
+-- 5.Temp Table of many Variables(Info)
 
+DROP TABLE IF EXISTS #info_table
+CREATE TABLE #info_table (
+						location varchar(50),
+						TotalDeaths float,
+						DeathsPerMillion float,
+						GDP float,
+						PovertyPercent float,
+						DiabetesPrevalence float,
+						Cardiovasc_Death_Rate float,
+						Life_Expectancy float,
+						Median_Age float,
+						Aged_65_older float,
+						Aged_75_older float
+				)
+				
+
+INSERT INTO #info_table
+SELECT 
+	location, 
+	MAX(total_deaths) as TotalDeaths,
+	MAX(total_deaths_per_million) as DeathsPerMillion,
+	MAX(gdp_per_capita) as GDP,
+	MAX(extreme_poverty) as Poverty,
+	diabetes_prevalence as DiabetesPrevalence, 
+	cardiovasc_death_rate as Cardiovasc_Death_Rate, 
+	life_expectancy as Life_Expectancy,
+	median_age as Median_Age,
+	aged_65_older as Aged_65_older,
+	aged_70_older as Aged_75_older
+FROM owid_covid_data ocd 
+GROUP BY 
+	location, 
+	diabetes_prevalence , 
+	cardiovasc_death_rate , 
+	life_expectancy ,
+	median_age,
+	aged_65_older,
+	aged_70_older
+
+	
 SELECT *
-FROM #restrict_table
-ORDER BY location, RestrictPower DESC
-
-
-
-
-
-
-
-
-
+FROM #info_table
 
 
 ------------------------------------------------------------------------------------------------
--- Question: Policy played role? argument variable: stringency_index
+-- 1.Question: Policy played role? argument variable: stringency_index
+------------------------------------------------------------------------------------------------
 SELECT iso_code, location, AVG(stringency_index)
 FROM PortfolioProject.dbo.owid_covid_data ocd 
 WHERE iso_code LIKE 'OWID_%'
@@ -89,12 +114,30 @@ GROUP BY location, gdp_per_capita
 
 
 
+------------------------------------------------------------------------------------------------
+-- 2.Question: How ICU helped people, did they recover? or high percentage died?
+------------------------------------------------------------------------------------------------
 
--- Question: How ICU helped people, did they recover? or high percentage died?
--- Question: How many days were borders in closure on high-middle and low income countries?
 
 
+------------------------------------------------------------------------------------------------
+-- 3.Question: How many days were borders in closure on high-middle and low income countries?
+------------------------------------------------------------------------------------------------
+
+
+
+
+------------------------------------------------------------------------------------------------
 -- Taking Data!
- 
+------------------------------------------------------------------------------------------------
 
+
+-- Taking the Restriction-Days Data
+SELECT *
+FROM #restrict_table
+ORDER BY location, RestrictPower ASC
+
+-- Taking the info_table Data
+SELECT *
+FROM #info_table
 
